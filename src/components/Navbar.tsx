337fe651a1e9b7887700968ea3fa421f194a5d5a
Ales <aleskruba@seznam.cz>
2024-06-23 13:12:42 +0200

    navbar update II

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,22 +25,24 @@ function Navbar() {
 }, [prevScrollPos]);
   return (
     <>
-      <nav className={` absolute bg-gray-200 dark:bg-gray-700  top-0 flex justify-between items-center w-full dark:text-yellow-500 text-yellow-800 md:px-4  md:py-4 md:text-2xl font-bold text-xl pb-4 `}>
+      <nav className={`relative bg-gray-200 dark:bg-gray-700  top-0 flex justify-between items-center w-full dark:text-yellow-500 text-yellow-800 md:px-4  md:py-4 md:text-2xl font-bold text-xl pb-4 `}>
         <div className='flex flex-col md:flex-row md:space-x-16 space-x-2 md:space-y-0 space-y-2 items-start md:items-center'>
           <Link to="/" className="md:w-[250px] w-[150px] block">
             <Image src={logo} alt="Logo" className='' />
           </Link>
 
-          <Link to="/traveltips" className="dark:hover:text-gray-300 hover:text-yellow-500 pl-4 md:pl-0">
-            TravelTips
-          </Link>
-          
-          <Link to="/spolucesty" className="dark:hover:text-gray-300 hover:text-yellow-500 pl-4 md:pl-0">
-            Spolucesty
-          </Link>
+            <div className='flex gap-6'>
+                <Link to="/traveltips" className="dark:hover:text-gray-300 hover:text-yellow-500 pl-4 md:pl-0">
+                    TravelTips
+                </Link>
+                
+                <Link to="/spolucesty" className="dark:hover:text-gray-300 hover:text-yellow-500 pl-4 md:pl-0">
+                    Spolucesty
+                </Link>
+             </div>
         </div>
 
-        <div className='flex space-x-4 items-center pr-6 md:pr-0'>
+        <div className='flex flex-col md:flex-row md:space-x-4 md:pt-2 items-start justify-start pr-6 md:pr-0 '>
           <Link to="/login" className="dark:hover:text-gray-300 hover:text-yellow-500">
             Login
           </Link>
@@ -49,15 +51,19 @@ function Navbar() {
             Register
           </Link>
 
-          <div className="dark:hover:text-gray-300 hover:text-yellow-500">
+        </div>
+
+        
+            <div className="dark:hover:text-gray-300 hover:text-yellow-500 absolute top-1 right-2">
             <ThemeComponent />
           </div>
-        </div>
+
       </nav>
 
       {/* BOTTOM NAVBAR */}
       <nav className={` ${visible ? 'hidden' : 'fixed'} bg-gray-200 dark:bg-gray-700 bottom-0 flex justify-between items-center  w-full dark:text-yellow-500 text-yellow-800 px-2 md:px-4 md:py-4 md:text-2xl font-bold text-base pb-4 `}>
         <div className='flex md:space-x-16 space-x-2 md:items-center'>
+
           <Link to="/traveltips" className="dark:hover:text-gray-300 hover:text-yellow-500">
             TravelTips
           </Link>
@@ -65,6 +71,7 @@ function Navbar() {
           <Link to="/spolucesty" className="dark:hover:text-gray-300 hover:text-yellow-500">
             Spolucesty
           </Link>
+        
         </div>
 
         <div className='flex space-x-4 items-center pr-6 md:pr-0'>
